a4ca48f12a9937e58392d82a6f5fdbeabca11c9c
Ali <72344723+alisabourii@users.noreply.github.com>
2025-09-05 17:33:02 +0300

    Add "new food" button

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 <div class="mainButtons">
     <button class="btn" onclick="location.href = 'menu.php'">Menu</button>
     <button class="btn" onclick="location.href = 'setting.php'">Settings</button>
-    <button class="btn" onclick="location.href = 'adding.php'">New Food</button>
+    <button class="btn" onclick="location.href = 'addfod.html'">New Food</button>
 </div>
 
 </body>
