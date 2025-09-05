4484996c3761caaaf9a0c8e59e9606d8d4576d77
Ali <72344723+alisabourii@users.noreply.github.com>
2025-09-05 20:39:33 +0300

    add "new food" backend

--- addfod.php
+++ addfod.php
@@ -1,2 +1,13 @@
 <?php
+$servername = "localhost";
+$username   = "root";
+$password   = "";
+$dbname     = "resturantdb";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+$id = $_GET['id'];
+$status = $_GET['status'];
+$conn->query("UPDATE `foods` SET stok='$status' WHERE `fd_id`=$id");
+
+//echo "Başarıyla güncellendi!";
 ?>
