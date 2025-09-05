83cde9c6c622d2bd37a22afb6b1a8a66a725adc2
Ali <72344723+alisabourii@users.noreply.github.com>
2025-09-05 17:31:29 +0300

    Add status get for sql

--- update.php
+++ update.php
@@ -6,7 +6,8 @@ $dbname     = "resturantdb";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 $id = $_GET['id'];
-$conn->query("UPDATE `foods` SET stok='1' WHERE `fd_id`=$id");
+$status = $_GET['status'];
+$conn->query("UPDATE `foods` SET stok='$status' WHERE `fd_id`=$id");
 
-echo "Başarıyla güncellendi!";
+//echo "Başarıyla güncellendi!";
 ?>
