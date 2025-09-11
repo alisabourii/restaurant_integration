eb33c4b95f5710047b1a9e3ac389aa8b9c500f99
Ali <72344723+alisabourii@users.noreply.github.com>
2025-09-11 12:57:59 +0300

    change css version

--- menu.php
+++ menu.php
@@ -22,7 +22,7 @@ $result = $conn->query($sql);
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="style.css" rel="stylesheet">
+    <link href="style.css?v=1.0" rel="stylesheet">
 </head>
 <body>
 <div class="mainButtons noPrint">
@@ -55,7 +55,4 @@ $result = $conn->query($sql);
 </div>
 
 </body>
-<script>
-
-</script>
 </html>
